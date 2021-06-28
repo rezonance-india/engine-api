6375caf5d342748614eb388dee6d63ef29657ab8
radioactive11 <roy.arijit2001@gmail.com>
2021-06-28 21:22:19 +0530

    completed search endpoint:sparkles:

--- src/main.py
+++ src/main.py
@@ -4,24 +4,45 @@ from pydantic import BaseModel
 
 import uvicorn
 
+import re
+
+from scripts import search
 
 app = FastAPI()
 
 
-class Search(BaseModel):
+class SearchResource(BaseModel):
     query: str
 
 @app.post('/search/tracks')
-def search_tracks(request_body: Search) -> dict:
+def _search_tracks(request_body: SearchResource) -> list:
     """search for tracks in database
 
     Args:
         request_body (Search): Schema of Request Body
     """
     data = request_body.query
+    data = re.sub(r'[^A-Za-z0-9 ]+', '', data)
+    result = search.search_tracks(data)
+
+    return result
 
 
+@app.post('/search/albums')
+def _search_albums(request_body: SearchResource) -> list:
+    data = request_body.query
+    data = re.sub(r'[^A-Za-z0-9 ]+', '', data)
+    result = search.search_tracks(data)
+
+    return result
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, debug=True)
+@app.post('/search/artists')
+def _search_artists(request_body: SearchResource) -> list:
+    data = request_body.query
+    data = re.sub(r'[^A-Za-z0-9 ]+', '', data)
+    result = search.search_artists(data)
+
+    return result
+
+
---
+++ src/scripts/search.py
@@ -0,0 +1,123 @@
+import sqlite3
+import sqlite_spellfix
+
+def search_tracks(param):
+    conn = sqlite3.connect("database/rezo.db")
+    conn.enable_load_extension(True)
+    conn.load_extension(sqlite_spellfix.extension_path())
+    cur = conn.cursor()
+    
+    correctedquery = []
+    for term in param.split():
+        spellfix_query = "SELECT word FROM tracks_spell WHERE word MATCH ? and top=1"
+        cur.execute(spellfix_query, (term,))
+        r = cur.fetchone()
+        correctedquery.append(r[0] if r is not None else term)  # correct the word if any match in the spellfix table; if no match, keep the word spelled as it is (then the search will give no result!)
+    
+    correctedquery = ' '.join(correctedquery)
+    correctedquery += '*'
+    cur.execute(
+        """
+        SELECT ref_id, track_name, a.album_name, art.artist_name, t.track_url, a.album_img, t.track_id
+        FROM tracks_fts4 t
+        INNER JOIN albums a
+        ON t.album_id = a.album_id
+        INNER JOIN artists art
+        ON a.artist_id = art.artist_id
+        WHERE tracks_fts4
+        MATCH ?
+        ORDER BY art.artist_popularity DESC
+        LIMIT 50
+        """, (correctedquery, )
+    )
+
+    dict_list = []
+    for item in cur.fetchmany(50):
+        item_dict = {
+            "ref_id": item[0],
+            "track_name": item[1],
+            "album_name": item[2],
+            "artist_name": item[3],
+            "track_url": item[4],
+            "album_image": item[5],
+            "track_id": item[6]
+        }
+        dict_list.append(item_dict)
+
+    unq_list = list({track['track_id']:track for track in dict_list}.values())
+    cur.close()
+    conn.close()
+    return unq_list
+
+
+def search_artists(param):
+        conn = sqlite3.connect("database/rezo.db")
+        cur = conn.cursor()
+    
+        cur.execute(
+                """
+                SELECT * FROM artists
+                WHERE artist_name
+                LIKE ('%' || ? || '%')
+                ORDER BY artist_popularity DESC
+                LIMIT 50
+                """, (param, )
+                    )
+        
+        dict_list = []
+        query_result = cur.fetchall()
+
+        for item in query_result:
+            item_dict = {
+                "artist_id": item[0],
+                "artist_name": item[1],
+                "artist_image": item[4],
+            }
+            dict_list.append(item_dict)
+        conn.close()
+        return dict_list
+
+
+
+def search_albums(param):
+    conn = sqlite3.connect("database/rezo.db")
+    conn.enable_load_extension(True)
+    conn.load_extension(sqlite_spellfix.extension_path())
+    cur = conn.cursor()
+    
+    correctedquery = []
+    for term in param.split():
+        spellfix_query = "SELECT word FROM albums_spell WHERE word MATCH ? and top=1"
+        cur.execute(spellfix_query, (term,))
+        r = cur.fetchone()
+        correctedquery.append(r[0] if r is not None else term)  # correct the word if any match in the spellfix table; if no match, keep the word spelled as it is (then the search will give no result!)
+    
+    correctedquery = ' '.join(correctedquery)
+    correctedquery += '*'
+
+    cur.execute(
+            """
+            SELECT * FROM albums_fts4
+            WHERE albums_fts4
+            MATCH ?
+            ORDER BY album_popularity DESC
+            LIMIT 50
+            """, (correctedquery, )
+                )
+    
+    dict_list = []
+    query_result = cur.fetchall()
+    for album in query_result:
+        album_dict = {
+            'artist_id': album[0],
+            'album_id': album[1],
+            'album_img': album[2],
+            'artist_name': album[4],
+            'album_name': album[5],
+        }
+        dict_list.append(album_dict)
+        
+    cur.close()
+    conn.close()
+    return dict_list
+
