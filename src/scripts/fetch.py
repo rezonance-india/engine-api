1d963f345fa7fce6162ea1ea559caa1797da9d30
radioactive11 <roy.arijit2001@gmail.com>
2021-06-29 12:16:26 +0530

    database fetch scripts:truck:

---
+++ src/scripts/fetch.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+
+def artist_albums(artist_id: str):
+    conn = sqlite3.connect("src/database/rezo.db")
+    cur = conn.cursor()
+
+    SQL = """
+          SELECT * FROM albums
+          WHERE artist_id = ?
+          ORDER by album_popularity DESC
+          LIMIT 50
+          """
+    cur.execute(SQL, (artist_id, ))
+
+    res = cur.fetchall()
+    dict_list = []
+    for album in res:
+        album_dict = {
+            "artist_name": album[0],
+            "album_name": album[1],
+            "album_id": album[2],
+            "artist_id": album[3],
+            "album_img": album[4],
+            "album_popularity": album[5]
+        }
+        dict_list.append(album_dict)
+    
+    cur.close()
+    conn.close()
+    return dict_list
+
+
+
+def albums_tracks(album_id: str):
+    conn = sqlite3.connect("src/database/rezo.db")
+    cur = conn.cursor()
+
+    SQL = """
+          SELECT * from tracks_fts4
+          WHERE album_id=?
+          LIMIT 50
+          """
+    cur.execute(SQL, (album_id, ))
+
+    tracks = cur.fetchall()
+    tracks_list = []
+    for track in tracks:
+        track_dict ={
+            "ref_id": track[0],
+            "track_name": track[3],
+            "track_id": track[2],
+            "track_url": track[5]
+        }
+        tracks_list.append(track_dict)
+
+    cur.close()
+    conn.close()
+    return tracks_list
+
+
