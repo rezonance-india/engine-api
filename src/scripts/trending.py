3c67fcf357b3c4c918e8f21eaa969edc27dfaf1f
Arijit Roy <roy.arijit2001@gmail.com>
2021-08-12 18:39:59 +0530

    handled no media url case:ambulance:

--- src/scripts/trending.py
+++ src/scripts/trending.py
@@ -12,6 +12,7 @@ def fetch_trending():
     if cached_data is not None:
         json_data = json.loads(cached_data)
         return json_data
+
     else:
         res = requests.get("https://www.jiosaavn.com/featured/english_chartbusters/1HiqW,xnqZTfemJ68FuXsA__").text
         id = res.split('"type":"playlist","id":"')[1].split('"')[0]
@@ -22,11 +23,15 @@ def fetch_trending():
         songs_json = res.text.encode().decode('unicode-escape')
         songs_json = json.loads(songs_json)
         songs_list = songs_json['songs']
-
+        
         result_list = []
 
         for item in songs_list:
-            song_prev_url = item['media_preview_url']
+            try:
+                song_prev_url = item['media_preview_url']
+            except Exception as e:
+                continue
+            
             song_id = song_prev_url.split('/')[3]
             song_url = song_prev_url.split('/')[4].split('_')[0]
             url = f"http://{secret.cdn_list[0]}.{secret.BASE_URL}/{song_id}/{song_url}_96.mp4"
